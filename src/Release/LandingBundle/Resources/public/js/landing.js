a7e3b9b37c5f4b622c1dc0a3b0b0a2952cae9fea
BenjaminRomeo <benjiplop@hotmail.fr>
2016-12-31 12:27:42 +0100

    Left Side

--- src/Release/LandingBundle/Resources/public/js/landing.js
+++ src/Release/LandingBundle/Resources/public/js/landing.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function($){
 	$(window).resize(function(){
-		
+
     });
 
+	$('.slider').slider({height: '100%'});
 	$(window).trigger('resize');
 });
+
--- web/bundles/main/js/menu.js
+++ web/bundles/main/js/menu.js
@@ -4,4 +4,19 @@ jQuery(document).ready(function($){
 		
 	});
 	$(window).trigger('resize');
+	$('body').on('click','.navTrigger',function(){
+		responsiveMenu();
+	});
 });
+
+function responsiveMenu()
+{
+	if($('.navTrigger').hasClass('opened'))
+	{
+		$('.navTrigger').addClass('closed').removeClass('opened');
+	}
+	else
+	{
+		$('.navTrigger').addClass('opened').removeClass('closed');
+	}
+}
